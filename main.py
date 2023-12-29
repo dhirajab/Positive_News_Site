10d5b1a5e551c87e8b562fbe76363348191b6cc1
Dhiraj <dhirajborkar002@gmail.com>
2023-12-29 17:06:45 +0530

    Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ content = request.json()
 # Access the article titles and description
 body = ""
 for article in content["articles"][:20]:
-      if article["title"] is not None:
-            body = body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+    if article["title"] is not None:
+        body = "Subject: Today's news" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
